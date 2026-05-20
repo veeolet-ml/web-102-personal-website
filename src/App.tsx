80e07e6cc68fd50121cad1da1578fdba18d477e4
veeolet-ml <veeolet.stan@gmail.com>
2026-05-20 16:34:24 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { GitHubActivity } from './components/GitHubActivity'
+
+function App() {
+
+  return (
+    <>
+      <GitHubActivity />
+    </>
+  )
+}
+
+export default App
