35504529f62ae3cee49628bce565d6f76d1aeac7
veeolet-ml <veeolet.stan@gmail.com>
2026-05-25 21:21:19 +0300

    Fix shadcn integration and add Carousel component

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,6 +1,7 @@
 import nifLover from "@/assets/niflover.png";
 import gameHub from "@/assets/gameHub.png";
 import HSI from "@/assets/hsi.png";
+import feedback from "@/assets/feedback.png";
 
 export type Project = {
   title: string;
@@ -34,5 +35,12 @@ export const projects: Project[] = [
       "C and Assembly demos for the HSI course @ UNSTPB, written for clarity and conciseness, to aid students in figuring everything out",
     image: HSI,
     href: "https://github.com/veeolet-ml/hardware-software-interface/",
+  },
+  {
+    title: "cs-pub-ro: feedback",
+    description: "The feedback analysis tool used by ACS @ UNSTPB",
+    details: "Contributed to the project by adding a Python linter",
+    image: feedback,
+    href: "https://github.com/veeolet-ml/cs-pub-ro-feedback/tree/add-python-linter",
   },
 ];
